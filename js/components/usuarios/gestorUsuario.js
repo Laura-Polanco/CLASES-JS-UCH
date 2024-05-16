5dff502a77be86b71c6fd6d4c5aa8ef8e59026c9
unknown <laupolancosa@uch.pe>
2024-05-16 02:40:43 -0500

    first commit

---
+++ js/components/usuarios/gestorUsuario.js
@@ -0,0 +1,115 @@
+import Servicios from './servicios.js';
+class GestorUsuarios {
+    constructor() {
+        this.servicios = new Servicios();
+        //todas las variables que deben inicializarse
+        this.token = '';
+        this.usuarios = [];
+        //LOS USUARIOS DE LA FUNCION GESTORUSUARIOS
+        this.init();
+    }
+    login() {
+        const usuario = $('#user').val();
+        const contrasena = $('#pass').val();
+        /*  
+            call(error, succss) {
+                if (error) { .. }
+                else { .. }
+            }
+            this.servicios.autenticar(usuario, contrasena, call){
+
+            }
+        */
+        this.servicios.autenticar(usuario, contrasena, (error, response) => {
+            if (error) {
+                alert('Usuario o contraseña incorrectos');
+            } else {
+                //this.usuarios.push(response.usuario);
+                //agrega un campo al array
+                console.log(response);
+                if (response.status == 200) {
+                    alert('¡Login exitoso!');
+                    this.token = response.token;
+                    this.cleanMain();
+                    this.mostrarUsuarios(this.token);
+                }
+            }
+        });
+    }
+    
+    mostrarUsuarios(token) {
+        this.servicios.obtenerUsuarios(token, (error, response) => {
+            if (error) {
+                console.error('Error al obtener usuarios:', error);
+            } else {
+                console.log(response);
+                this.renderizarUsuarios(response);
+            }
+        });
+    }
+    cleanMain() {
+        $("#mainlogin").html("");
+    }
+    renderizarUsuarios(usuarios) {
+        //usuarios ==> Array
+        $('#mainlogin').append(`
+        <table>
+            <tr>
+                <th>Nombre</th>
+                <th>Edad</th>
+                <th>DNI</th>
+                <th>Estado Civil</th>
+                <th>Foto</th>
+            </tr>
+        `);
+        
+        usuarios.forEach(usuario => {
+            const color = usuario.age < 18 ? "red" : "green"; // cambio edad por age
+            $('#mainlogin table').append(`
+                <tr>
+                    <td>${usuario.name}</td>
+                    <td style="color:${color};">${usuario.age}</td> // cambio edad por age
+                    <td>${usuario.dni}</td>
+                    <td>${usuario.estado}</td> // cambio estadoCivil por estado
+                    <td class="usuario"><img src="${usuario.foto}" alt="" style="width: 100px; height: 100px;"></td> <!-- ajusta el ancho y alto aquí -->
+                </tr>
+            `);
+        });
+        
+        $('#mainlogin').append(`</table>`);
+    }
+
+
+
+
+
+    renderLogin() {
+        const templatelogin = `<div class="inputLogin">
+            <div class="input">
+                <label>Usuario</label>
+                <input type="text" id="user" />
+            </div>
+            <div class="input">
+                <label>Password</label>
+                <input type="password" id="pass" />
+            </div>
+            <div class="input">
+                <button type="submit" class="btn" id="btLogin">Logear</button>
+            </div>
+        </div>`;
+        $("#mainlogin").append(templatelogin);
+    }
+    // funciones para IMPRIMIR vistas
+    render() {
+        this.renderLogin();
+    }
+    init() {
+        this.render();
+        //otras funcionalidades
+        $('#btLogin').on('click', () => {
+            this.login();
+        });
+    }
+}
+
+export default GestorUsuarios;
